48be228c54898586a183e54a675c6af98578d233
Charith99IT <charithmadusankha99@gmail.com>
2022-10-29 04:43:25 +0530

    Firebase integrated

---
+++ app/src/main/java/com/example/q_app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.q_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //var firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
+    }
+}
